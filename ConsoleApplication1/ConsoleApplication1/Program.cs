f37287d559cfbcf2fa52415381896a45ba2ca210
huangchong@qq.com <huangchong@qq.com>
2012-02-28 23:39:29 +0800

    hello world

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,6 +10,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             Console.WriteLine("hello git");
+            Console.WriteLine("hello world");
         }
     }
 }
