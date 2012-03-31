f571dd694540e6b0ca0083598d5be63513e1e4a4
June <ouch_e@qq.com>
2012-03-31 22:47:11 +0800

    Merge branch 'master' of github.com:RedAntCN/LearnSite
    Conflicts:
    	test.txt

---
+++ Model/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections;
+
+namespace Model
+{
+    public class CommomRowDataModel
+    {
+        protected Hashtable data;
+        public CommomRowDataModel()
+        {
+            data = new Hashtable();
+        }
+        public Object this[String ColumnName]
+        {
+            get
+            {
+                return data[ColumnCount];
+            }
+            set
+            {
+                data[ColumnCount] = value;
+            }
+        }
+        public void SetValue(String ColumnName, Object value)
+        {
+            data[ColumnName] = value;
+        }
+        public Object GetValue(String ColumnName)
+        {
+            return data[ColumnName];
+        }
+        public int ColumnCount { get { return data.Count; } }
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Model.CommomRowDataModel c = new Model.CommomRowDataModel();
+            c.SetValue("FirstName", "Ouch");
+            c.SetValue("LastName", "OO");
+            Console.WriteLine(c.ColumnCount);
+            Console.WriteLine(c.GetValue("FirstName").ToString());
+            Console.ReadKey();
+            
+        }
+    }
+}
